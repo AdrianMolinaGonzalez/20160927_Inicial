d49e78a1d127525fc309de82496e85f0ec1e9e59
ADRI\Adrián <adrimolinag11@gmail.com>
2016-09-29 21:07:15 +0200

    visualización de parametros de entrada

--- src/java/es/albarregas/servlets/CicloVida.java
+++ src/java/es/albarregas/servlets/CicloVida.java
@@ -7,6 +7,7 @@ package es.albarregas.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import static java.lang.System.out;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,18 +22,39 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "CicloVida", urlPatterns = {"/ciclo"})
 public class CicloVida extends HttpServlet {
 
+   
+    
 public void init(ServletConfig config){
-    System.out.println("init()");
+  
+   
+    
 }
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        System.out.println("service()");
+         PrintWriter out = response.getWriter();   
+         out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Par&aacute;metros</title>");             
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Par&aacute;metros</h1>");
+            java.util.Enumeration<String> parametros = request.getParameterNames();
+            while(parametros.hasMoreElements())
+            {
+                String elemento=parametros.nextElement();
+                String valor= request.getParameter(elemento);
+                out.println(elemento + " - " + valor + "<br/>");
+            }
+          
+            out.println("</body>");
+            out.println("</html>");
     }
 
  public void destroy()
  {
-      System.out.println("service()");
+      System.out.println("destroy()");
  }
 
 }
